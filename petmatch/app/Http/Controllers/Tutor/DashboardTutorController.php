02f6b14bcd00c4a1c0860e6f105ca24756e897b9
Gaby Lopes <developergabrielly@gmail.com>
2025-06-29 19:31:46 -0300

    chore(controller): ajusta namespaces dos controllers conforme estrutura de pastas
    - Corrige namespace dos controllers Admin e Tutor
    - Atualiza imports para evitar conflitos
    - Mantém os métodos ainda sem implementação

--- petmatch/app/Http/Controllers/Tutor/DashboardTutorController.php
+++ petmatch/app/Http/Controllers/Tutor/DashboardTutorController.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Tutor;
+use App\Http\Controllers\Controller;  
 use Illuminate\Http\Request;
 
 class DashboardTutorController extends Controller
