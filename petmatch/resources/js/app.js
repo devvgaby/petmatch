527a5a6af75b69c0d80d39ce798aa2e4aa3f98ad
Gaby Lopes <developergabrielly@gmail.com>
2025-07-02 15:05:59 -0300

    chore(deps): adiciona dompdf, bootstrap, chart.js
    - Atualiza composer.json e composer.lock para novas dependências PHP
    - Configura imports JS para Bootstrap, Chart.js
    - Atualiza content-hash do composer.lock

--- petmatch/resources/js/app.js
+++ petmatch/resources/js/app.js
@@ -1,4 +1,9 @@
-import './bootstrap';
+import "./bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap'; 
+
+import Chart from 'chart.js/auto';
+window.Chart = Chart;
 
 import Alpine from 'alpinejs';
 
